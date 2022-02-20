a001338a65d813fab19aae69d9ab25c405c3b91c
Nathan Hoad <nathan@nathanhoad.net>
2022-02-20 11:55:18 +1000

    Fix translation for responses

--- addons/dialogue_manager/dialogue_response.gd
+++ addons/dialogue_manager/dialogue_response.gd
@@ -7,6 +7,6 @@ var next_id: String
 
 
 func _init(data: Dictionary, should_translate: bool = true) -> void:
-	prompt = tr(data.get("text")) if should_translate else data.get("text")
+	prompt = tr(data.get("translation_key")) if should_translate else data.get("text")
 	replacements = data.get("replacements")
 	next_id = data.get("next_id")
