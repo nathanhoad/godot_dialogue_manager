a5b3016ad486e62e8d2566272f7b61742b669755
Nathan Hoad <nathan@nathanhoad.net>
2022-03-09 20:59:59 +1000

    Force resolution when running the test scene
    Related to #33

--- addons/dialogue_manager/views/test_scene.gd
+++ addons/dialogue_manager/views/test_scene.gd
@@ -5,6 +5,8 @@ onready var settings = $Settings
 
 
 func _ready():
+	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, Vector2(1920, 1080))
+	
 	DialogueManager.connect("dialogue_finished", self, "_on_dialogue_finished")
 	
 	var title = settings.get_editor_value("run_title")
