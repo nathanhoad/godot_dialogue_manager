4cf6c9757463981978053cbb1ec569e9536e4eaa
Nathan Hoad <nathan@nathanhoad.net>
2022-03-26 19:23:04 +1000

    Fix invalid set index warning
    I'm not sure how this causes it but removing the type reference seems to fix an error when trying to set properties on a DialogueResource.

--- addons/dialogue_manager/dialogue_resource.gd
+++ addons/dialogue_manager/dialogue_resource.gd
@@ -3,7 +3,6 @@ class_name DialogueResource
 
 
 const Constants = preload("res://addons/dialogue_manager/constants.gd")
-const DialogueLine = preload("res://addons/dialogue_manager/dialogue_line.gd")
 
 
 export(int) var syntax_version
@@ -21,13 +20,13 @@ func _init():
 	lines = {}
 
 
-func get_next_dialogue_line(title: String) -> DialogueLine:
+func get_next_dialogue_line(title: String):
 	# NOTE: For some reason get_singleton doesn't work here so we have to get creative
 	var tree: SceneTree = Engine.get_main_loop()
 	if tree:
 		var dialogue_manager = tree.current_scene.get_node_or_null("/root/DialogueManager")
 		if dialogue_manager != null:
 			return dialogue_manager.get_next_dialogue_line(title, self)
-	
+
 	assert(false, "The \"DialogueManager\" autoload does not appear to be loaded.")
 	return null
