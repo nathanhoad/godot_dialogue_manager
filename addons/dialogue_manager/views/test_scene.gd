04bf0a164a84f46f27c7a9590a5398a28fcc9291
Roy Riggs <furroy@gmail.com>
2022-07-26 08:13:49 -0400

    my custom test scene changes

--- addons/dialogue_manager/views/test_scene.gd
+++ addons/dialogue_manager/views/test_scene.gd
@@ -5,16 +5,17 @@ onready var settings = $Settings
 
 
 func _ready():
-	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, Vector2(1280, 720))
-	OS.window_size = Vector2(1280, 720)
-	OS.window_position = (OS.get_screen_size() - OS.window_size) * 0.5
-	OS.window_fullscreen = false
+#	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, Vector2(1920, 1080))
+#	OS.window_size = Vector2(1920, 1080)
+#	OS.window_position = (OS.get_screen_size() - OS.window_size) * 0.5
+#	OS.window_fullscreen = false
 	
 	DialogueManager.connect("dialogue_finished", self, "_on_dialogue_finished")
 	
 #	var title = settings.get_editor_value("run_title")
 	var full_path = settings.get_editor_value("run_resource")
 	var dialogue_resource = load(full_path)
+	SaveGame.current_id = "start"
 	State.switch_scene(full_path.get_basename().get_file())
 #	DialogueManager.show_example_dialogue_balloon(title, dialogue_resource)
 
@@ -23,4 +24,5 @@ func _ready():
 
 
 func _on_dialogue_finished():
-	get_tree().quit()
+#	get_tree().quit()
+	pass
