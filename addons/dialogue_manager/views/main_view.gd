590a478b02b22eda283ee1036b61b72efb369d6b
Chevon <chevgc@gmail.com>
2022-12-02 20:50:47 -0500

    Add a setting for creating blank dialogue files (#112)

--- addons/dialogue_manager/views/main_view.gd
+++ addons/dialogue_manager/views/main_view.gd
@@ -179,19 +179,20 @@ func new_file(path: String, content: String = "") -> void:
 	
 	var file: FileAccess = FileAccess.open(path, FileAccess.WRITE)
 	if content == "":
-		file.store_string("\n".join([
-			"~ this_is_a_node_title",
-			"",
-			"Nathan: [[Hi|Hello|Howdy]], this is some dialogue.",
-			"Nathan: Here are some choices.",
-			"- First one",
-				"\tNathan: You picked the first one.",
-			"- Second one",
-				"\tNathan: You picked the second one.",
-			"- Start again => this_is_a_node_title",
-			"- End the conversation => END",
-			"Nathan: For more information see the online documentation."
-		]))
+		if DialogueSettings.get_setting("new_with_template", true):
+			file.store_string("\n".join([
+				"~ this_is_a_node_title",
+				"",
+				"Nathan: [[Hi|Hello|Howdy]], this is some dialogue.",
+				"Nathan: Here are some choices.",
+				"- First one",
+					"\tNathan: You picked the first one.",
+				"- Second one",
+					"\tNathan: You picked the second one.",
+				"- Start again => this_is_a_node_title",
+				"- End the conversation => END",
+				"Nathan: For more information see the online documentation."
+			]))
 	else:
 		file.store_string(content)
 		
