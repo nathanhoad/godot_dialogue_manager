f9a2c20c8760d4430929bac5197198c28c9adb0c
Nathan Hoad <nathan@nathanhoad.net>
2023-03-19 12:55:49 +1000

    Tidy up how DialogueLabel accepts input

--- addons/dialogue_manager/dialogue_label.gd
+++ addons/dialogue_manager/dialogue_label.gd
@@ -27,59 +27,58 @@ var dialogue_line: DialogueLine:
 var last_wait_index: int = -1
 var last_mutation_index: int = -1
 var waiting_seconds: float = 0
-var is_typing: bool = false
-var has_finished: bool = false
+
+var is_typing: bool = false:
+	set(value):
+		if is_typing != value and value == false:
+			finished_typing.emit()
+		is_typing = value
+	get:
+		return is_typing
 
 
 func _process(delta: float) -> void:
-	if is_typing:
+	if self.is_typing:
 		# Type out text
 		if visible_ratio < 1:
-			# If cancel is pressed then skip typing it out
-			if Input.is_action_just_pressed(skip_action):
-				# Run any inline mutations that haven't been run yet
-				for i in range(visible_characters, get_total_character_count()):
-					dialogue_line.mutate_inline_mutations(i)
-				visible_characters = get_total_character_count()
-				has_finished = true
-				emit_signal("finished_typing")
-				return
-			
-			# Otherwise, see if we are waiting
+			# See if we are waiting
 			if waiting_seconds > 0:
 				waiting_seconds = waiting_seconds - delta
 			# If we are no longer waiting then keep typing
 			if waiting_seconds <= 0:
 				type_next(delta, waiting_seconds)
 		else:
-			is_typing = false
-			if has_finished == false:
-				has_finished = true
-				emit_signal("finished_typing")
+			self.is_typing = false
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if self.is_typing and visible_ratio < 1 and event.is_action_pressed(skip_action):
+		# Run any inline mutations that haven't been run yet
+		for i in range(visible_characters, get_total_character_count()):
+			dialogue_line.mutate_inline_mutations(i)
+		visible_characters = get_total_character_count()
+		self.is_typing = false
+		finished_typing.emit()
+		
 
 # Start typing out the text
 func type_out() -> void:
 	text = dialogue_line.text
 	visible_characters = 0
-	has_finished = false
+	self.is_typing = true
 	waiting_seconds = 0
 	
 	# Text isn't calculated until the next frame
 	await get_tree().process_frame
 	
 	if get_total_character_count() == 0:
-		emit_signal("finished_typing")
+		self.is_typing = false
 	elif seconds_per_step == 0:
-		is_typing = false
 		# Run any inline mutations
 		for i in range(0, get_total_character_count()):
 			dialogue_line.mutate_inline_mutations(i)
 		visible_characters = get_total_character_count()
-		emit_signal("finished_typing")
-	else:
-#		percent_per_index = 100.0 / float(get_total_character_count()) / 100.0
-		is_typing = true
+		self.is_typing = false
 
 
 # Type out the next character(s)
@@ -98,13 +97,13 @@ func type_next(delta: float, seconds_needed: float) -> void:
 	if last_wait_index != visible_characters and additional_waiting_seconds > 0:
 		last_wait_index = visible_characters
 		waiting_seconds += additional_waiting_seconds
-		emit_signal("paused_typing", dialogue_line.get_pause(visible_characters))
+		paused_typing.emit(dialogue_line.get_pause(visible_characters))
 	else:
 		visible_characters += 1
 		seconds_needed += seconds_per_step * (1.0 / dialogue_line.get_speed(visible_characters))
 		if seconds_needed > delta:
 			waiting_seconds += seconds_needed
 			if visible_characters < get_total_character_count():
-				emit_signal("spoke", text[visible_characters - 1], visible_characters - 1, dialogue_line.get_speed(visible_characters))
+				spoke.emit(text[visible_characters - 1], visible_characters - 1, dialogue_line.get_speed(visible_characters))
 		else:
 			type_next(delta, seconds_needed)
