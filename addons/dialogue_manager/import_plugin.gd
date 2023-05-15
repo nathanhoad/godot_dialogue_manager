2984b9e0a1d33b351440041c106254beaf539270
Nathan Hoad <nathan@nathanhoad.net>
2023-05-15 20:21:11 +1000

    Stop params error on export
    Fixes #185

--- addons/dialogue_manager/import_plugin.gd
+++ addons/dialogue_manager/import_plugin.gd
@@ -50,11 +50,16 @@ func _get_preset_name(preset_index: int) -> String:
 
 
 func _get_import_options(path: String, preset_index: int) -> Array:
-	return []
+	# When the options array is empty there is a misleading error on export
+	# that actually means nothing so let's just have an invisible option.
+	return [{
+		name = "defaults",
+		default_value = true
+	}]
 
 
 func _get_option_visibility(path: String, option_name: StringName, options: Dictionary) -> bool:
-	return true
+	return false
 
 
 func _import(source_file: String, save_path: String, options: Dictionary, platform_variants: Array[String], gen_files: Array[String]) -> Error:
@@ -64,41 +69,41 @@ func _import(source_file: String, save_path: String, options: Dictionary, platfo
 func compile_file(path: String, resource_path: String, will_cascade_cache_data: bool = true) -> Error:
 	# Get the raw file contents
 	if not FileAccess.file_exists(path): return ERR_FILE_NOT_FOUND
-	
+
 	var file: FileAccess = FileAccess.open(path, FileAccess.READ)
 	var raw_text: String = file.get_as_text()
-	
+
 	# Parse the text
 	var parser: DialogueManagerParser = DialogueManagerParser.new()
 	var err: Error = parser.parse(raw_text)
 	var data: DialogueManagerParseResult = parser.get_data()
 	var errors: Array[Dictionary] = parser.get_errors()
 	parser.free()
-	
+
 	if err != OK:
 		printerr("%d errors found in %s" % [errors.size(), path])
 		editor_plugin.add_errors_to_dialogue_file_cache(path, errors)
 		return err
-		
+
 	# Get the current addon version
 	var config: ConfigFile = ConfigFile.new()
 	config.load("res://addons/dialogue_manager/plugin.cfg")
 	var version: String = config.get_value("plugin", "version")
-	
+
 	# Save the results to a resource
 	var resource: DialogueResource = DialogueResource.new()
 	resource.set_meta("dialogue_manager_version", version)
-	
+
 	resource.titles = data.titles
 	resource.first_title = data.first_title
 	resource.character_names = data.character_names
 	resource.lines = data.lines
-	
+
 	if will_cascade_cache_data:
 		editor_plugin.add_to_dialogue_file_cache(path, resource_path, data)
 
 	err = ResourceSaver.save(resource, resource_path)
 
-	emit_signal("compiled_resource", resource)
-	
+	compiled_resource.emit(resource)
+
 	return err
