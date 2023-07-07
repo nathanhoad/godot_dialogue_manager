71b3ba61f7f0c381be05121e713e88be20d6c0ab
Nathan Hoad <nathan@nathanhoad.net>
2023-07-07 20:43:44 +1000

    Fix release order checker

--- addons/dialogue_manager/components/update_button.gd
+++ addons/dialogue_manager/components/update_button.gd
@@ -26,10 +26,10 @@ var on_before_refresh: Callable = func(): return true
 func _ready() -> void:
 	hide()
 	apply_theme()
-	
+
 	# Check for updates on GitHub
 	check_for_update()
-	
+
 	# Check again every few hours
 	timer.start(60 * 60 * 12)
 
@@ -49,14 +49,14 @@ func version_to_number(version: String) -> int:
 
 func apply_theme() -> void:
 	var color: Color = get_theme_color("success_color", "Editor")
-	
+
 	if needs_reload:
 		color = get_theme_color("error_color", "Editor")
 		icon = get_theme_icon("Reload", "EditorIcons")
 		add_theme_color_override("icon_normal_color", color)
 		add_theme_color_override("icon_focus_color", color)
 		add_theme_color_override("icon_hover_color", color)
-	
+
 	add_theme_color_override("font_color", color)
 	add_theme_color_override("font_focus_color", color)
 	add_theme_color_override("font_hover_color", color)
@@ -71,17 +71,21 @@ func check_for_update() -> void:
 
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	if result != HTTPRequest.RESULT_SUCCESS: return
-	
+
 	var current_version: String = get_version()
-	
+
 	# Work out the next version from the releases information on GitHub
 	var response = JSON.parse_string(body.get_string_from_utf8())
 	if typeof(response) != TYPE_ARRAY: return
-	
-	var next_version: String = response[0].tag_name.substr(1)
-	if version_to_number(next_version) > version_to_number(current_version):
-		download_update_panel.next_version_release = response[0]
-		text = DialogueConstants.translate("update.available").format({ version = next_version })
+
+	# GitHub releases are in order of creation, not order of version
+	var versions = (response as Array).filter(func(release):
+		var version: String = release.tag_name.substr(1)
+		return version_to_number(version) > version_to_number(current_version)
+	)
+	if versions.size() > 0:
+		download_update_panel.next_version_release = versions[0]
+		text = DialogueConstants.translate("update.available").format({ version = versions[0].tag_name.substr(1) })
 		show()
 
 
@@ -102,12 +106,12 @@ func _on_download_dialog_close_requested() -> void:
 
 func _on_download_update_panel_updated(updated_to_version: String) -> void:
 	download_dialog.hide()
-	
+
 	needs_reload_dialog.dialog_text = DialogueConstants.translate("update.needs_reload")
 	needs_reload_dialog.ok_button_text = DialogueConstants.translate("update.reload_ok_button")
 	needs_reload_dialog.cancel_button_text = DialogueConstants.translate("update.reload_cancel_button")
 	needs_reload_dialog.popup_centered()
-	
+
 	needs_reload = true
 	text = DialogueConstants.translate("update.reload_project")
 	apply_theme()
