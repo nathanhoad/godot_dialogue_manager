992fc81cf37cb7028c2ec9192d31521979a240d4
Nathan Hoad <nathan@nathanhoad.net>
2023-08-12 13:26:19 +1000

    Add more code docs

--- addons/dialogue_manager/dialogue_resource.gd
+++ addons/dialogue_manager/dialogue_resource.gd
@@ -1,20 +1,36 @@
 @icon("./assets/icon.svg")
 
+## A collection of dialogue lines for use with [code]DialogueManager[/code].
 class_name DialogueResource extends Resource
 
 
-const DialogueManager = preload("res://addons/dialogue_manager/dialogue_manager.gd")
+const _DialogueManager = preload("res://addons/dialogue_manager/dialogue_manager.gd")
 
 
+## A map of titles and the lines they point to.
 @export var titles: Dictionary = {}
+
+## A list of character names.
 @export var character_names: PackedStringArray = []
+
+## The first title in the file.
 @export var first_title: String = ""
+
+## A map of the encoded lines of dialogue.
 @export var lines: Dictionary = {}
 
 
-func get_next_dialogue_line(title: String, extra_game_states: Array = [], mutation_behaviour: DialogueManager.MutationBehaviour = DialogueManager.MutationBehaviour.Wait) -> DialogueLine:
+## Get the next printable line of dialogue, starting from a referenced line ([code]title[/code] can
+## be a title string or a stringified line number). Runs any mutations along the way and then returns
+## the first dialogue line encountered.
+func get_next_dialogue_line(title: String, extra_game_states: Array = [], mutation_behaviour: _DialogueManager.MutationBehaviour = _DialogueManager.MutationBehaviour.Wait) -> DialogueLine:
 	return await Engine.get_singleton("DialogueManager").get_next_dialogue_line(self, title, extra_game_states, mutation_behaviour)
 
 
+## Get the list of any titles found in the file.
 func get_titles() -> PackedStringArray:
 	return titles.keys()
+
+
+func _to_string() -> String:
+	return "<DialogueResource titles=\"%s\">" % [",".join(titles.keys())]
