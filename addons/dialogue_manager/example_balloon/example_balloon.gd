fb67f2a89a10d7d9d28dc73d07355f9b67ee1c37
Nathan Hoad <nathan@nathanhoad.net>
2023-09-06 11:42:54 +1000

    Allow clicking to skip typing

--- addons/dialogue_manager/example_balloon/example_balloon.gd
+++ addons/dialogue_manager/example_balloon/example_balloon.gd
@@ -198,6 +198,12 @@ func _on_response_gui_input(event: InputEvent, item: Control) -> void:
 
 
 func _on_balloon_gui_input(event: InputEvent) -> void:
+	# If the user clicks on the balloon while it's typing then skip typing
+	if dialogue_label.is_typing and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		get_viewport().set_input_as_handled()
+		dialogue_label.skip_typing()
+		return
+
 	if not is_waiting_for_input: return
 	if dialogue_line.responses.size() > 0: return
 
