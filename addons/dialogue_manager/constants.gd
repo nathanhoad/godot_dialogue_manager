62f259b0a064f61142ba39ff33ca24a7d5a626b8
憨憨羊の宇航鸽鸽 <13538773578@163.com>
2023-10-25 18:43:25 +0800

    Fixed and updated Chinese translations (#341)

--- addons/dialogue_manager/constants.gd
+++ addons/dialogue_manager/constants.gd
@@ -252,8 +252,9 @@ static func get_error_message(error: int) -> String:
 
 
 static func translate(string: String) -> String:
-	var language: String = TranslationServer.get_tool_locale().substr(0, 2)
+	var language: String = TranslationServer.get_tool_locale()
 	var translations_path: String = "res://addons/dialogue_manager/l10n/%s.po" % language
-	var fallback_translations_path: String = "res://addons/dialogue_manager/l10n/en.po"
-	var translations: Translation = load(translations_path if FileAccess.file_exists(translations_path) else fallback_translations_path)
+	var fallback_translations_path: String = "res://addons/dialogue_manager/l10n/"+TranslationServer.get_tool_locale().substr(0, 2)+".po"
+	var en_translations_path: String = "res://addons/dialogue_manager/l10n/en.po"
+	var translations: Translation = load(translations_path if FileAccess.file_exists(translations_path) else (fallback_translations_path if FileAccess.file_exists(fallback_translations_path) else en_translations_path))
 	return translations.get_message(string)
