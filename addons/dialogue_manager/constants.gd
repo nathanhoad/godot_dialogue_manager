3a6826f8217b0efaf7bb05e2b575e4e210143381
Nathan Hoad <nathan@nathanhoad.net>
2023-11-08 20:06:27 +1000

    Clean up constant

--- addons/dialogue_manager/constants.gd
+++ addons/dialogue_manager/constants.gd
@@ -38,7 +38,6 @@ const TOKEN_ERROR = "error"
 # Line types
 
 const TYPE_UNKNOWN = "unknown"
-const TYPE_IMPORT = "import"
 const TYPE_RESPONSE = "response"
 const TYPE_TITLE = "title"
 const TYPE_CONDITION = "condition"
