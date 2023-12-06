9f3d1b44f54bf6db57808bb61d268228dc753ca1
Nathan Hoad <nathan@nathanhoad.net>
2023-12-06 23:14:29 +1000

    Fix missing error when indenting titles (#407)

--- tests/test.gd
+++ tests/test.gd
@@ -2,7 +2,12 @@ class_name AbstractTest extends Node
 
 
 func parse(text: String) -> DialogueManagerParseResult:
-	return DialogueManagerParser.parse_string(text, "")
+	var parser: DialogueManagerParser = DialogueManagerParser.new()
+	parser.parse(text, "")
+	var data: DialogueManagerParseResult = parser.get_data()
+	parser.free()
+
+	return data
 
 
 func create_resource(text: String) -> DialogueResource:
