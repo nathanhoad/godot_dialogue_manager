b1a46edc22a2a91876c5975ad93fa9a4ca6a3827
Nathan Hoad <nathan@nathanhoad.net>
2024-01-20 13:44:30 +1000

    Simplify/centralise typing skip handling

--- addons/dialogue_manager/example_balloon/example_balloon.gd
+++ addons/dialogue_manager/example_balloon/example_balloon.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
+## The action to use for advancing the dialogue
+const NEXT_ACTION = &"ui_accept"
+
+## The action to use to skip typing the dialogue
+const SKIP_ACTION = &"ui_cancel"
+
 
 @onready var balloon: Panel = %Balloon
 @onready var character_label: RichTextLabel = %CharacterLabel
@@ -22,6 +28,8 @@ var will_hide_balloon: bool = false
 var dialogue_line: DialogueLine:
 	set(next_dialogue_line):
 		is_waiting_for_input = false
+		balloon.focus_mode = Control.FOCUS_ALL
+		balloon.grab_focus()
 
 		# The dialogue has finished so close the balloon
 		if not next_dialogue_line:
@@ -105,11 +113,14 @@ func _on_mutated(_mutation: Dictionary) -> void:
 
 
 func _on_balloon_gui_input(event: InputEvent) -> void:
-	# If the user clicks on the balloon while it's typing then skip typing
-	if dialogue_label.is_typing and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		get_viewport().set_input_as_handled()
-		dialogue_label.skip_typing()
-		return
+	# See if we need to skip typing of the dialogue
+	if dialogue_label.is_typing:
+		var mouse_was_clicked: bool = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()
+		var skip_button_was_pressed: bool = event.is_action_pressed(SKIP_ACTION)
+		if mouse_was_clicked or skip_button_was_pressed:
+			get_viewport().set_input_as_handled()
+			dialogue_label.skip_typing()
+			return
 
 	if not is_waiting_for_input: return
 	if dialogue_line.responses.size() > 0: return
@@ -119,7 +130,7 @@ func _on_balloon_gui_input(event: InputEvent) -> void:
 
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == 1:
 		next(dialogue_line.next_id)
-	elif event.is_action_pressed("ui_accept") and get_viewport().gui_get_focus_owner() == balloon:
+	elif event.is_action_pressed(NEXT_ACTION) and get_viewport().gui_get_focus_owner() == balloon:
 		next(dialogue_line.next_id)
 
 
