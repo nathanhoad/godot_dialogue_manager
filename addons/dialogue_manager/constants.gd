5c2b5f4aad6de1c663a0c93aa9b814f25290d097
Nathan Hoad <nathan@nathanhoad.net>
2024-01-28 15:36:06 +1000

    Defer evaluating assert messages (#462)

--- addons/dialogue_manager/constants.gd
+++ addons/dialogue_manager/constants.gd
@@ -255,9 +255,11 @@ static func get_error_message(error: int) -> String:
 
 
 static func translate(string: String) -> String:
+	var base_path = DialogueResource.new().get_script().resource_path.get_base_dir()
+
 	var language: String = TranslationServer.get_tool_locale()
-	var translations_path: String = "res://addons/dialogue_manager/l10n/%s.po" % language
-	var fallback_translations_path: String = "res://addons/dialogue_manager/l10n/"+TranslationServer.get_tool_locale().substr(0, 2)+".po"
-	var en_translations_path: String = "res://addons/dialogue_manager/l10n/en.po"
+	var translations_path: String = "%s/l10n/%s.po" % [base_path, language]
+	var fallback_translations_path: String = "%s/l10n/%s.po" % [base_path, TranslationServer.get_tool_locale().substr(0, 2)]
+	var en_translations_path: String = "%s/l10n/en.po" % base_path
 	var translations: Translation = load(translations_path if FileAccess.file_exists(translations_path) else (fallback_translations_path if FileAccess.file_exists(fallback_translations_path) else en_translations_path))
 	return translations.get_message(string)
