7bbc53d1250b787d864df54e6516ae29f4a81892
Nathan Hoad <nathan@nathanhoad.net>
2024-07-17 21:30:04 +1000

    Add inspector plugin

---
+++ addons/dialogue_manager/inspector_plugin.gd
@@ -0,0 +1,21 @@
+@tool
+extends EditorInspectorPlugin
+
+
+const DialogueEditorProperty = preload("./components/editor_property/editor_property.gd")
+
+
+func _can_handle(object) -> bool:
+	if object is GDScript: return false
+	if not object is Node: return false
+	if "name" in object and object.name == "Dialogue Manager": return false
+	return true
+
+
+func _parse_property(object: Object, type: int, name: String, hint_type: int, hint_string: String, usage_flags: int, wide: bool) -> bool:
+	if hint_string == "DialogueResource" or ("dialogue" in name.to_lower() and hint_string == "Resource"):
+		var property_editor = DialogueEditorProperty.new()
+		add_property_editor(name, property_editor)
+		return true
+
+	return false
