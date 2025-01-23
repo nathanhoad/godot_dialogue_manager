0fd7a9e4ec857d97d182c60dfb411c3e45551c80
Nathan Hoad <nathan@nathanhoad.net>
2025-01-23 18:42:09 +1100

    Fix responses menu keyboard input (#733)

--- addons/dialogue_manager/dialogue_reponses_menu.gd
+++ addons/dialogue_manager/dialogue_reponses_menu.gd
@@ -130,11 +130,11 @@ func _on_response_mouse_entered(item: Control) -> void:
 func _on_response_gui_input(event: InputEvent, item: Control, response) -> void:
 	if "Disallowed" in item.name: return
 
-	get_viewport().set_input_as_handled()
-
 	if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
+		get_viewport().set_input_as_handled()
 		response_selected.emit(response)
 	elif event.is_action_pressed(&"ui_accept" if next_action.is_empty() else next_action) and item in get_menu_items():
+		get_viewport().set_input_as_handled()
 		response_selected.emit(response)
 
 
