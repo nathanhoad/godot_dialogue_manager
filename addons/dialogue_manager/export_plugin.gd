eeec1475657b3d56bb1b56ae4048a402ddd51bc3
Nathan Hoad <nathan@nathanhoad.net>
2025-03-07 12:36:37 +1000

    Trim dialogue editor from exported builds (#815)

---
+++ addons/dialogue_manager/export_plugin.gd
@@ -0,0 +1,20 @@
+class_name DMExportPlugin extends EditorExportPlugin
+
+const IGNORED_PATHS = [
+	"/assets",
+	"/components",
+	"/l10n",
+	"/views"
+]
+
+func _export_file(path: String, type: String, features: PackedStringArray) -> void:
+	var plugin_path: String = Engine.get_meta("DialogueManagerPlugin").get_plugin_path()
+
+	# Ignore any editor stuff
+	for ignored_path: String in IGNORED_PATHS:
+		if path.begins_with(plugin_path + ignored_path):
+			skip()
+
+	# Ignore C# stuff it not using dotnet
+	if path.begins_with(plugin_path) and not DMSettings.check_for_dotnet_solution() and path.ends_with(".cs"):
+		skip()
