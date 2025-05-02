1361ed3b3f628ee388387c95e5f4aca0a18d91bc
Nathan Hoad <nathan@nathanhoad.net>
2025-05-03 09:35:04 +1000

    Add a test for C# state

--- tests/CSharpState.cs
+++ tests/CSharpState.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Godot;
 
 public partial class CSharpState : Node
@@ -5,4 +6,17 @@ public partial class CSharpState : Node
   public const int SOME_CONSTANT = 2;
 
   [Export] public int SomeValue = 0;
+
+
+  public async Task<Variant> GetAsyncValue()
+  {
+    await ToSignal(GetTree().CreateTimer(0.2f), Timer.SignalName.Timeout);
+    return 100;
+  }
+
+
+  public async Task LongMutation()
+  {
+    await ToSignal(GetTree().CreateTimer(1.0f), Timer.SignalName.Timeout);
+  }
 }
