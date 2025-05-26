03384442a455115d499847cff6f3b4e8dc292b35
OpenSauce <lawleycobhc@gmail.com>
2025-05-26 13:28:02 +0100

    Fix caching

--- addons/dialogue_manager/constants.gd
+++ addons/dialogue_manager/constants.gd
@@ -221,10 +221,11 @@ static func get_error_message(error: int) -> String:
 
 static func translate(string: String) -> String:
 	var locale: String = TranslationServer.get_tool_locale()
-	if _current_locale != locale:
+	if _current_translation == null or _current_locale != locale:
 		var base_path: String = new().get_script().resource_path.get_base_dir()
 		var translation_path: String = "%s/l10n/%s.po" % [base_path, locale]
 		var fallback_translation_path: String = "%s/l10n/%s.po" % [base_path, locale.substr(0, 2)]
 		var en_translation_path: String = "%s/l10n/en.po" % base_path
 		_current_translation = load(translation_path if FileAccess.file_exists(translation_path) else (fallback_translation_path if FileAccess.file_exists(fallback_translation_path) else en_translation_path))
+		_current_locale = locale
 	return _current_translation.get_message(string)
