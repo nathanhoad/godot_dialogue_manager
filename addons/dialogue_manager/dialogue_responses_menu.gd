cf2168fcd4a28f74cba4c8e377e412548c983745
Nathan Hoad <nathan@nathanhoad.net>
2025-09-12 19:56:34 +1000

    Add options for configuring response menu focus

--- addons/dialogue_manager/dialogue_responses_menu.gd
+++ addons/dialogue_manager/dialogue_responses_menu.gd
@@ -17,51 +17,22 @@ signal response_selected(response: Control)
 ## The action for accepting a response (is possibly overridden by parent dialogue balloon).
 @export var next_action: StringName = &""
 
+## Automatically set up focus neighbours when the responses list changes.
+@export var auto_configure_focus: bool = true
+
+## Automatically focus the first item when showing.
+@export var auto_focus_first_item: bool = true
+
 ## Hide any responses where [code]is_allowed[/code] is false
 @export var hide_failed_responses: bool = false
 
 ## The list of dialogue responses.
 var responses: Array = []:
-	get:
-		return responses
 	set(value):
 		responses = value
-
-		# Remove any current items
-		for item in get_children():
-			if item == response_template: continue
-
-			remove_child(item)
-			item.queue_free()
-
-		# Add new items
-		if responses.size() > 0:
-			for response in responses:
-				if hide_failed_responses and not response.is_allowed: continue
-
-				var item: Control
-				if is_instance_valid(response_template):
-					item = response_template.duplicate(DUPLICATE_GROUPS | DUPLICATE_SCRIPTS | DUPLICATE_SIGNALS)
-					item.show()
-				else:
-					item = Button.new()
-				item.name = "Response%d" % get_child_count()
-				if not response.is_allowed:
-					item.name = item.name + &"Disallowed"
-					item.disabled = true
-
-				# If the item has a response property then use that
-				if "response" in item:
-					item.response = response
-				# Otherwise assume we can just set the text
-				else:
-					item.text = response.text
-
-				item.set_meta("response", response)
-
-				add_child(item)
-
-			_configure_focus()
+		_apply_responses()
+	get:
+		return responses
 
 # The previously focused item in this menu.
 var _previously_focused_item: Control = null
@@ -69,7 +40,7 @@ var _previously_focused_item: Control = null
 
 func _ready() -> void:
 	visibility_changed.connect(func():
-		if visible and get_menu_items().size() > 0:
+		if auto_focus_first_item and visible and get_menu_items().size() > 0:
 			var first_item: Control = get_menu_items()[0]
 			if first_item.is_inside_tree():
 				first_item.grab_focus()
@@ -92,11 +63,8 @@ func get_menu_items() -> Array:
 	return items
 
 
-#region Internal
-
-
-# Prepare the menu for keyboard and mouse navigation.
-func _configure_focus() -> void:
+## Prepare the menu for keyboard and mouse navigation.
+func configure_focus() -> void:
 	var items = get_menu_items()
 	for i in items.size():
 		var item: Control = items[i]
@@ -128,7 +96,52 @@ func _configure_focus() -> void:
 		item.gui_input.connect(_on_response_gui_input.bind(item, item.get_meta("response")))
 
 	_previously_focused_item = items[0]
-	items[0].grab_focus()
+
+	if auto_focus_first_item:
+		items[0].grab_focus()
+
+
+#region Internal
+
+
+# Set up the visual side of things.
+func _apply_responses() -> void:
+	# Remove any current items
+	for item in get_children():
+		if item == response_template: continue
+
+		remove_child(item)
+		item.queue_free()
+
+	# Add new items
+	if responses.size() > 0:
+		for response in responses:
+			if hide_failed_responses and not response.is_allowed: continue
+
+			var item: Control
+			if is_instance_valid(response_template):
+				item = response_template.duplicate(DUPLICATE_GROUPS | DUPLICATE_SCRIPTS | DUPLICATE_SIGNALS)
+				item.show()
+			else:
+				item = Button.new()
+			item.name = "Response%d" % get_child_count()
+			if not response.is_allowed:
+				item.name = item.name + &"Disallowed"
+				item.disabled = true
+
+			# If the item has a response property then use that
+			if "response" in item:
+				item.response = response
+			# Otherwise assume we can just set the text
+			else:
+				item.text = response.text
+
+			item.set_meta("response", response)
+
+			add_child(item)
+
+		if auto_configure_focus:
+			configure_focus()
 
 
 #endregion
