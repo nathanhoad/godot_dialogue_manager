f0f5ed4d0d2f14697349a3d3eae446c4be2ccf5d
Nathan Hoad <nathan@nathanhoad.net>
2026-02-25 20:59:09 +1000

    Add "auto" option for label list view

--- addons/dialogue_manager/components/label_list.gd
+++ addons/dialogue_manager/components/label_list.gd
@@ -7,6 +7,11 @@ signal label_selected(label: String)
 const REGION_ICON: Texture2D = preload("../assets/region.svg")
 const LABEL_ICON: Texture2D = preload("../assets/label.svg")
 
+const VIEW_REGIONS_LABELS: String = "regions+labels"
+const VIEW_REGIONS: String = "regions"
+const VIEW_LABELS: String = "labels"
+const VIEW_AUTO: String = "auto"
+
 
 var labels: PackedStringArray:
 	set(next_labels):
@@ -47,12 +52,23 @@ func select_label(label: String) -> void:
 
 func apply_filter() -> void:
 	list.clear()
+
+	var view_setting: String = DMSettings.get_user_value("label_list_view", "regions+labels")
+	# If auto then see if there are regions in the list to then default to regions but if there
+	# are none default to labels instead
+	if view_setting == VIEW_AUTO:
+		view_setting = VIEW_LABELS
+		for label: String in labels:
+			if label.begins_with("#"):
+				view_setting = VIEW_REGIONS
+				break
+
 	for label: String in labels:
 		if filter == "" or filter.to_lower() in label.to_lower():
 			if label.begins_with("#"):
-				if "regions" in DMSettings.get_user_value("label_list_view", "regions+labels"):
+				if VIEW_REGIONS in view_setting:
 					list.add_item(label.strip_edges().substr(1), region_icon)
-			elif "labels" in DMSettings.get_user_value("label_list_view", "regions+labels"):
+			elif VIEW_LABELS in view_setting:
 				list.add_item(label.strip_edges(), label_icon)
 
 
@@ -91,21 +107,25 @@ func _on_menu_button_about_to_popup() -> void:
 	var menu: PopupMenu = menu_button.get_popup()
 	menu.clear()
 	menu.add_radio_check_item(DMConstants.translate(&"Regions & Labels"), 0)
-	menu.set_item_checked(0, DMSettings.get_user_value("label_list_view", "regions+labels") == "regions+labels")
+	menu.set_item_checked(0, DMSettings.get_user_value("label_list_view", VIEW_REGIONS_LABELS) == VIEW_REGIONS_LABELS)
 	menu.add_radio_check_item(DMConstants.translate(&"Regions"), 1)
-	menu.set_item_checked(1, DMSettings.get_user_value("label_list_view", "regions+labels") == "regions")
+	menu.set_item_checked(1, DMSettings.get_user_value("label_list_view", VIEW_REGIONS_LABELS) == VIEW_REGIONS)
 	menu.add_radio_check_item(DMConstants.translate(&"Labels"), 2)
-	menu.set_item_checked(2, DMSettings.get_user_value("label_list_view", "regions+labels") == "labels")
+	menu.set_item_checked(2, DMSettings.get_user_value("label_list_view", VIEW_REGIONS_LABELS) == VIEW_LABELS)
+	menu.add_radio_check_item(DMConstants.translate(&"Auto"), 3)
+	menu.set_item_checked(3, DMSettings.get_user_value("label_list_view", VIEW_REGIONS_LABELS) == VIEW_AUTO)
 
 
 func _on_menu_button_id_pressed(id: int) -> void:
 	match id:
 		0:
-			DMSettings.set_user_value("label_list_view", "regions+labels")
+			DMSettings.set_user_value("label_list_view", VIEW_REGIONS_LABELS)
 		1:
-			DMSettings.set_user_value("label_list_view", "regions")
+			DMSettings.set_user_value("label_list_view", VIEW_REGIONS)
 		2:
-			DMSettings.set_user_value("label_list_view", "labels")
+			DMSettings.set_user_value("label_list_view", VIEW_LABELS)
+		3:
+			DMSettings.set_user_value("label_list_view", VIEW_AUTO)
 	apply_filter()
 
 #endregion
