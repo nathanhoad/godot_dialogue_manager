9fced2dc788f58c456572c36f7e0436b7389f646
Nathan Hoad <nathan@nathanhoad.net>
2026-03-17 21:18:37 +1000

    Make blank translations fall back to English (#1144)

--- addons/dialogue_manager/constants.gd
+++ addons/dialogue_manager/constants.gd
@@ -127,6 +127,7 @@ const ERR_MISSING_RESOURCE_FOR_AUTOSTART = 143
 
 static var _current_locale: String = ""
 static var _current_translation: Translation
+static var _en_translation: Translation
 
 
 ## Get the error message
@@ -230,5 +231,7 @@ static func translate(string: String) -> String:
 		var fallback_translation_path: String = "%s/l10n/%s.po" % [base_path, locale.substr(0, 2)]
 		var en_translation_path: String = "%s/l10n/en.po" % base_path
 		_current_translation = load(translation_path if FileAccess.file_exists(translation_path) else (fallback_translation_path if FileAccess.file_exists(fallback_translation_path) else en_translation_path))
+		_en_translation = load(en_translation_path)
 		_current_locale = locale
-	return _current_translation.get_message(string)
+	var message: String = _current_translation.get_message(string)
+	return message if not message.is_empty() else _en_translation.get_message(string)
