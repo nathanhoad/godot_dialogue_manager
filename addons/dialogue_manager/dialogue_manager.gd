ca8fcd3a08d84fa4c46875859d87d1a8ba8f2620
Nathan Hoad <nathan@nathanhoad.net>
2026-08-18 18:03:22 +1000

    Remove extra tr()

--- addons/dialogue_manager/dialogue_manager.gd
+++ addons/dialogue_manager/dialogue_manager.gd
@@ -739,7 +739,7 @@ func translate(data: Dictionary) -> String:
 	if TranslationServer.get_loaded_locales().size() == 0:
 		return data.text
 
-	var static_id: String = tr(data.get(&"static_id", data.text))
+	var static_id: String = data.get(&"static_id", data.text)
 	if static_id.is_empty() or static_id == data.text:
 		return tr(data.text, "dialogue")
 	else:
